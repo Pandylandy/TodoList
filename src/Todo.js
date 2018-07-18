523b060f7874263b6c8432da66004a1514f035e7
pandylandy <adelik21979@gmail.com>
2018-07-18 14:42:19 +0300

    filters done

--- src/Todo.js
+++ src/Todo.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Table, Input, Button, Divider } from 'antd';
-import { addTaskAction, closeTaskAction, activateTaskAction, showActiveAction, showClosedAction, showAllAction } from "./action";
+import { addTaskAction, closeTaskAction, activateTaskAction} from "./action";
 import { Select } from 'antd';
 
 
 const Option = Select.Option;
-
-function handleChange(value) {
-  console.log(`selected ${value}`);
-
-}
-
-
-
 const Search = Input.Search;
 
 
@@ -23,68 +15,86 @@ const todoStates = {
     all: 'All',
   }
 ;
+const getTodos = (data, key) => {
+    if (todoStates[key] === 'All') {
+        return data;
+    }
+    else if (todoStates[key] === 'Active') {
+        return data.filter(o => o.status === 'open');
+    }
+    else { return data.filter(o => o.status === 'done');
+    }
 
-const ToDo = ({ addTask, closeTask, activateTask, showActive, showAll, showClosed, data }) => {
 
-  const onClick = (key) => {
-        if (todoStates[key] === 'Active') {
-          console.log('^^', showActive);
-            return showActive();
-        }
-        else  if (todoStates[key] === 'Done') {
-            return showClosed();
-        }
-        else    {
-            return showAll();
-        }
-    };
-  const columns = [{
-    title: 'Id',
-    dataIndex: 'id',
-    key: 'id',
-  },
-    {
-    title: 'Status',
-    dataIndex: 'status',
-    key: 'status',
-  },
-    {
-    title: 'Text',
-    dataIndex: 'text',
-    key: 'text',
-    },
-    { title: 'Action',
-      dataIndex: 'id',
-      key: 'action',
-      render: (x) => (
-        <span>
-          <Button href="" onClick={() => closeTask(x)}>Close</Button>
-          <Divider type="vertical" />
-          <Button href="" onClick={() => activateTask(x)}>Open</Button>
-        </span>
-      )
-    },
 
-  ];
-  return (
-    <div align="center">
-      <Select defaultValue="active" style={{ width: 120 }} onChange={onClick}>
-        {Object.keys(todoStates).map((key, i) =>
-          <Option key={i} value={key}>{todoStates[key]}</Option>
-        )}
-    </Select>
-
-    <Search
-      placeholder="type to add"
-      onSearch={addTask}
-      enterButton={<Button> OK</Button>}
-    />
-
-    <Table columns={columns} dataSource={data}/>
-    </div>
-  )
 };
 
+let keyFilter = 'all';
+
+class ToDo extends React.Component {
+
+    render() {
+
+        const {addTask, closeTask, activateTask, data} = this.props;
+
+        const Todos = getTodos(data, keyFilter);
+
+        const onClick = (key) => {
+            keyFilter = key;
+            this.setState({todo: getTodos(data, keyFilter)})
+
+        };
+        const columns = [{
+            title: 'Id',
+            dataIndex: 'id',
+            key: 'id',
+        },
+        {
+            title: 'Status',
+            dataIndex: 'status',
+            key: 'status',
+        },
+        {
+            title: 'Text',
+            dataIndex: 'text',
+            key: 'text',
+        },
+        {
+            title: 'Action',
+            dataIndex: 'id',
+            key: 'action',
+            render: (x) => (
+                <span>
+                  <Button href="" onClick={() => closeTask(x)}>Close</Button>
+                  <Divider type="vertical"/>
+                  <Button href="" onClick={() => activateTask(x)}>Open</Button>
+                </span>
+            )
+            },
+        ];
+        return (
+            <div>
+                <div align="center">
+                    <Select defaultValue="active" style={{width: 120}} onChange={onClick}>
+                        {Object.keys(todoStates).map((key, i) =>
+                            <Option key={i} value={key}>{todoStates[key]}</Option>
+                        )}
+                    </Select>
+
+                    <Search
+                        placeholder="type to add"
+                        onSearch={addTask}
+                        enterButton={<Button> OK</Button>}
+                    />
+                </div>
+                <div>
+                    <Table columns={columns} dataSource={Todos}/>
+                </div>
+            </div>
+        )
+
+    }
+    }
 
 const mapStateToProps = state => ({
   data: state.todo,
@@ -94,9 +104,6 @@ const mapDispatchToProps = dispatch => ({
   addTask: (text) => dispatch(addTaskAction(text)),
   closeTask: (id) => dispatch(closeTaskAction(id)),
   activateTask : (id) => dispatch(activateTaskAction(id)),
-  showActive: () => dispatch(showActiveAction()),
-  showClosed: () => dispatch(showClosedAction()),
-  showAll: () => dispatch(showAllAction())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ToDo);
